da239bc962227d17e764ded9cc4a731b7c14b8db
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2024-08-24 04:38:46 +0300

    Partially designed the desktop navigation bar

--- src/app/desktop-ui/desktop-ui.component.ts
+++ src/app/desktop-ui/desktop-ui.component.ts
@@ -1,33 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { 
   TuiIcon,
   TuiButton,
   TuiDataList,
-  TuiTitle
+  TuiTitle,
+  TuiTextfield,
+  TuiHint,
+  TuiDropdown
  } from '@taiga-ui/core';
+ 
 import {
   TuiNavigation,
 } from '@taiga-ui/layout'
+import {
+  TuiInputModule,
+  TuiTextfieldControllerModule
+} from '@taiga-ui/legacy'
 
 import {
   TuiFade,
-  TuiBadge
+  TuiBadge,
+  TuiChevron,
+  TuiDataListDropdownManager,
+  TuiBadgeNotification
 } from '@taiga-ui/kit';
+import {ReactiveFormsModule,FormControl, Validators } from '@angular/forms';
+import { TuiObscured } from '@taiga-ui/cdk/directives/obscured';
+import { TuiActiveZone } from '@taiga-ui/cdk/directives/active-zone';
 @Component({
   selector: 'app-desktop-ui',
   standalone: true,
   imports: [
+    TuiHint,
+    ReactiveFormsModule,
+    TuiDataListDropdownManager,
     TuiNavigation,
+    TuiBadgeNotification,
     TuiIcon,
+    TuiFade,
     TuiButton,
     TuiDataList,
     TuiTitle,
     TuiFade,
-    TuiBadge
+    TuiBadge,
+    TuiChevron,
+    TuiInputModule,
+    TuiTextfieldControllerModule,
+    TuiTextfield,
+    TuiDataList,
+    TuiDropdown,
+    TuiObscured,
+    TuiActiveZone,
+    
   ],
   templateUrl: './desktop-ui.component.html',
-  styleUrl: './desktop-ui.component.less'
+  styleUrl: './desktop-ui.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DesktopUiComponent {
   expanded=true
+  search=new FormControl ('',[Validators.required])
 }
