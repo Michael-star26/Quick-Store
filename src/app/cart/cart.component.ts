60725baa6d2173f0d8bca8372034e9ec35209472
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2024-10-20 02:34:22 +0300

    solved the gallery problem involving thumbnails

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -23,7 +23,9 @@ import {
 import { CommonModule } from '@angular/common';
 import { TuiInputNumberModule,tuiInputNumberOptionsProvider,TuiTextfieldControllerModule } from '@taiga-ui/legacy';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { GALLERY_CONFIG, GalleryConfig , GalleryModule, Gallery, GalleryRef, ImageItem} from 'ng-gallery';
+import { GALLERY_CONFIG, GalleryConfig , GalleryModule, Gallery, GalleryRef, ImageItem,} from 'ng-gallery';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 @Component({
   selector: 'app-cart',
   standalone: true,
@@ -66,7 +68,7 @@ tuiInputNumberOptionsProvider({
   provide: GALLERY_CONFIG,
   useValue: {
     autoHeight: true,
-    imageSize: 'cover'
+    imageSize: 'cover',
   } as GalleryConfig
 }
 ]
@@ -85,16 +87,19 @@ export class CartComponent implements OnInit{
   cartQuantity=new FormGroup({
     quantity:new FormControl(1)
   })
-
+  
   ngOnInit(): void {
-    const galleryRef = this.gallery.ref('myGallery');
-    galleryRef.load([
-      new ImageItem({ src: '/brand3.png', thumb: 'brand3.png',alt:'xyz',type:'png' }),
-      new ImageItem({ src: '/brand3.png', thumb: 'brand3.png',alt:'xyz' }),
-      new ImageItem({ src: '/brand3.png', thumb: 'brand3.png',alt:'xyz' }),
-      new ImageItem({ src: '/images/samsung8.jpg', thumb: '/images/samsung8.jpg' }),
-      new ImageItem({ src: '/images/samsung8.jpg', thumb: '/images/samsung8.jpg' }),
-      new ImageItem({ src: '/images/samsung8.jpg', thumb: '/images/samsung8.jpg' }),
+      const galleryRef = this.gallery.ref('myGallery');
+      galleryRef.load([
+      new ImageItem({ src: '/images/clothe1.jpg', thumb: 'images/clothe1.jpg' }),
+      new ImageItem({ src: '/images/clothe2.jpg', thumb: 'images/clothe2.jpg' }),
+      new ImageItem({ src: '/images/clothe3.jpg', thumb: 'images/clothe3.jpg' }),
+      new ImageItem({ src: '/images/clothe4.jpg', thumb: 'images/clothe4.jpg' }),
+      new ImageItem({ src: '/images/clothe5.jpg', thumb: 'images/clothe5.jpg' }),
+      new ImageItem({ src: '/images/laptop1.jpg', thumb: '/images/laptop1.jpg' }),
+      new ImageItem({ src: '/images/laptop3.jpg', thumb: '/images/laptop3.jpg' }),
+      new ImageItem({ src: '/images/samsung2.jpg', thumb: '/images/samsung2.jpg' }),
+      new ImageItem({ src: '/images/samsung4.jpg', thumb: '/images/samsung4.jpg' }),
       // ... more items
     ])
   }
